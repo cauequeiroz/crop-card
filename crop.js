d1f97bf1a9240363a58201ecc669655eb1d8097a
Caue Queiroz <cauenqueiroz@gmail.com>
2017-07-17 01:20:51 -0300

    Store final object into a global variable

--- crop.js
+++ crop.js
@@ -1,7 +1,7 @@
 const fs  = require('fs');
 const pagarme = fs.readFileSync('pagarme.js', 'utf-8');
 
-const final = '{ elo:' + pagarme.split('elo:')[1].split('};')[0] + '}';
+const final = 'var CREDITCARD_BINDS = { elo:' + pagarme.split('elo:')[1].split('};')[0] + '}';
 const stream = fs.createWriteStream('./bin.js');
 
 stream.once('open', () => {
